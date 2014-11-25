16150586bfcea110e8a936e181d67e73f8e6e3e1
Orobey Oleg <o.orobey@intetics.com>
2014-11-25 22:08:38 +0200

    asd

--- GroupProject/GroupProject/main.c
+++ GroupProject/GroupProject/main.c
@@ -5,7 +5,12 @@
 char login[100], password[100];
 int type;
 int countOfUsers;
-
+int findId, bFindId;//
+sqlite3 *db;
+char str[100];
+int isTrue = 1;
+char *zErrMsg = 0;
+int rc;
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
    int i;
@@ -18,6 +23,70 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
 
 
 
+static int findIdForNewAccount(void *NotUsed, int argc, char **argv, char **azColName){//
+   int i;
+   if(bFindId)
+	   findId++;
+   for(i=0; i<argc; i++){
+      if(bFindId && findId-1!=atoi(argv[i])){
+		  --findId;
+		  bFindId = 0;
+	  }
+   }
+   i++;
+   return 0;
+}
+
+static int addAccount(){
+	char query[1000];
+	char clientCode[100], startBalance[100];
+	char accFindNewId[] = "SELECT BANK_ACCOUNTS.account_id FROM BANK_ACCOUNTS ORDER BY BANK_ACCOUNTS.account_id ASC;";
+	char accInsertOperation[] = "INSERT INTO BANK_ACCOUNTS (balance, account_id, client_id, type_of_account, totalTransactions) VALUES ('%s','%d', '%s', 'actual', '0');";
+	char ans;
+
+	bFindId = 1; findId = 1;
+	rc = sqlite3_exec(db,accFindNewId, findIdForNewAccount, 0, &zErrMsg);
+	printf("\nNew account number : %d\n", findId);
+	printf("Input client code<= ");
+	scanf("%s", clientCode);
+	printf("Input start balance<= ");
+	scanf("%s", startBalance);
+	printf("Data about account:\nAccount id: %d\nclient code: '%s'\nStart balance: '%s'\nAdd new account y/n?",findId,clientCode,startBalance);
+	scanf("%c",&ans);
+	scanf("%c",&ans);
+	if(ans == 'y'){
+		sprintf_s(query, 1000, accInsertOperation, startBalance, findId,clientCode);
+		rc = sqlite3_exec(db,query, callback, 0, &zErrMsg);
+		printf("\nOK\n");
+		}
+	else{
+		printf("\nCancel\n");
+	}
+}
+
+static int delAccount(){
+	char query[1000], idChar[100], ans;
+	char showDataAboutAccount[] = "SELECT * FROM BANK_ACCOUNTS WHERE BANK_ACCOUNTS.account_id = '%s';";
+	char deleteAccount[] = "DELETE FROM BANK_ACCOUNTS WHERE BANK_ACCOUNTS.account_id = '%s';";
+	char delCard[] = "DELETE FROM Card WHERE Card.accNum = '%s';";
+	printf("Input account id <= ");
+	scanf("%s", idChar);
+	sprintf_s(query, 1000, showDataAboutAccount, idChar);
+	printf("Delete this account? y/n\n");
+	rc = sqlite3_exec(db,query, callback, 0, &zErrMsg);
+	scanf("%c",&ans);
+	scanf("%c",&ans);
+	if(ans == 'y'){
+		sprintf_s(query, 1000, deleteAccount, idChar);
+		rc = sqlite3_exec(db,query, callback, 0, &zErrMsg);
+		sprintf_s(query, 1000, delCard, idChar);
+		rc = sqlite3_exec(db,query, callback, 0, &zErrMsg);
+		printf("\nOK\n");
+	}
+	else{
+		printf("\nCancel\n");
+	}
+}
 
 
 static int getTypeOfUser(void *NotUsed, int argc, char **argv, char **azColName){
@@ -35,13 +104,7 @@ static int getCountOfUsersWithLogin(void *NotUsed, int argc, char **argv, char *
 }
 int main()
 {
-	sqlite3 *db;
-	char str[100];
-	int isTrue = 1;
 	int a;
-    char *zErrMsg = 0;
-    int rc;
-	
 	
 	char query[1000];
 	char format[] = "SELECT * FROM Personal WHERE Login = '%s' AND Password = '%s';";
@@ -86,9 +149,14 @@ int main()
 		while(isTrue){
 			scanf("%s", str);//fgets(str, 100, stdin);
 			if(!strcmp(str, "help")){
-				printf("\n\n");
+				printf("\naddAccount - add account\ndelAccount - delete account\n");
 			}
-
+			else if(!strcmp(str, "addAccount" )){
+				addAccount();
+			}
+			else if(!strcmp(str,"delAccount")){
+				delAccount();
+			}
 			else if(!strcmp(str,"exit")){
 				isTrue = 0;
 				printf("By!");
