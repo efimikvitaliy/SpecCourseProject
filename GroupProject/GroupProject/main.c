abc9bf9e63f968685d28fd662e2b07390f335532
Orobey Oleg <o.orobey@intetics.com>
2014-11-26 11:39:21 +0300

    help fixed

--- GroupProject/GroupProject/main.c
+++ GroupProject/GroupProject/main.c
@@ -459,8 +459,7 @@ int main()
 		while(isTrue){
 			scanf("%s", str);//fgets(str, 100, stdin);
 			if(!strcmp(str,"help")){
-					printf("\nshowAccounts - show accounts of the client\nshowBalance - show balance of the account or card\n");
-
+				printf("\nshowAccounts - show accounts of the client\nshowBalance - show balance of the account or card\n");
 				}
 			else if (!strcmp(str, msgShowAccounts)){
 				showAccounts();
