a8e4255a3c6fbcfe405b61db423ea49e6b569125
Orobey Oleg <o.orobey@intetics.com>
2014-12-09 02:25:52 +0200

    tests added

---
+++ GroupProject/GroupProject.Tests/tests.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int successfullTest(){
+	return 0;
+}
+
+int failureTest(){
+	return 1;
+}
+
+int main()
+{
+	int res = 0;
+	res += successfullTest();
+	//res += failureTest();
+
+	if (res != 0){
+		printf("Tests failed!");
+	}
+	else{
+		printf("Tests succeeded!");
+	}
+
+	return res;
+}
