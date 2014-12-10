03ee874fea1c5eb470974a74810652437116a330
efimikvitaliy <vitaliy1993@mail.ru>
2014-12-10 15:29:09 +0300

    upd

--- GroupProject/GroupProject.Tests/tests.c
+++ GroupProject/GroupProject.Tests/tests.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-//#include "../GroupProject/main.c"
+#include "../GroupProject/Header.h"
 
 
 
@@ -13,11 +13,25 @@ int failureTest(){
 	return 1;
 }
 
+
+int testDeleteClient() {
+	int ret = 0;
+	if (deleteClient("deleteClient asd") != 1) {
+		printf("!!!!!!!!!!!!!!!!!!!!!!!!DeleteClien with correct id\n");
+		ret = 0;
+	}
+	else {
+		printf("------------------------DeleteClien with incorrect id\n");
+		ret = 1;
+	}
+	return ret;
+}
+
 int main()
 {
 	int res = 0;
 	res += successfullTest();
-	res += failureTest();
+	res += testDeleteClient();
 
 	if (res != 0){
 		printf("Tests failed!");
